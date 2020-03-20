24c1b6161204586647c01c96e5f3098918a2f210
welbertFC <welbert.jsj@gmail.com>
2020-03-20 12:19:44 -0300

    Analise de caracteres
    top 10 de analise de caracteres

--- Analytical_characters.py
+++ Analytical_characters.py
@@ -10,9 +10,11 @@ class AnalyticalCharacteres(Txt):
 
     def character_frequency(self):
         aparicoes = Counter(self._texto.lower())
-        vezes_aparece = aparicoes.most_common()
-        for caractere, vezes_que_aparece in vezes_aparece:
-            print(f'caractere: {caractere} == {vezes_que_aparece}')
+        vezes_aparece = aparicoes.most_common(10)
+        return vezes_aparece
+
+
+
 
     def character_frequency_proportion(self):
 
@@ -20,9 +22,10 @@ class AnalyticalCharacteres(Txt):
         total_de_caracteres = sum(aparicoes.values())
         proporcoes = [(letra, frquencia / total_de_caracteres) for letra, frquencia in aparicoes.items()]
         proporcoes = Counter(dict(proporcoes))
-        mais_comuns = proporcoes.most_common(15)
+        mais_comuns = proporcoes.most_common(10)
         for caractere, proporcao in mais_comuns:
-            print ('Caractere: {} => {:.2f}%'.format(caractere,proporcao *100))
+            return caractere, proporcao 
+            #print ('Caractere: {} => {:.2f}%'.format(caractere,proporcao *100))
 
     def format(self):
 
--- main.py
+++ main.py
@@ -1,38 +1,59 @@
 from flask import Flask, render_template, request, redirect, url_for
-
+from collections import Counter
 from Txt import Txt
 import Sum
-
+import Analytical_characters
 
 app = Flask(__name__)
 app.secret_key = 'senha'
+
+class Analise_caracteres():
+    def __init__(self, caracteres, vezes_que_aparece):
+        self.caracteres = caracteres
+        self.vezes_que_aparece = vezes_que_aparece
+
+
+caracteres = []
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/resposta' )
+
+@app.route('/resposta')
 def resposta():
     sum_character = request.args.get('sum_character')
     character_sem_espaco = request.args.get('character_sem_espaco')
     sum_palavras = request.args.get('sum_palavras')
 
-    return render_template('resposta.html',  sum_character = sum_character,
-                           character_sem_espaco = character_sem_espaco,
-                           sum_palavras = sum_palavras)
 
-@app.route('/analise_soma', methods = ['POST',])
+
+    return render_template('resposta.html', sum_character=sum_character,
+                           character_sem_espaco=character_sem_espaco,
+                           sum_palavras=sum_palavras,
+                           fequencia=caracteres)
+
+
+@app.route('/analise_soma', methods=['POST', ])
 def analise_soma():
+    #analise de soma
     texto = request.form['text']
     objeto_texto = Txt(texto)
     character_sem_espaco = Sum.Sum.sum_character_sem_espaco(objeto_texto)
     sum_character = Sum.Sum.sum_character(objeto_texto)
     sum_palavras = Sum.Sum.sum_palavras(objeto_texto)
 
-    return redirect(url_for('resposta',character_sem_espaco = character_sem_espaco,
-                            sum_character =  sum_character,
-                            sum_palavras = sum_palavras))
+    #anlise de frequencia de caracteres 
+    character_frequency = Analytical_characters.AnalyticalCharacteres.character_frequency(objeto_texto)
+    for caractere, vezes_que_aparece in character_frequency:
+        analise = Analise_caracteres(caractere,vezes_que_aparece)
+        caracteres.append(analise)
+
+
+    return redirect(url_for('resposta', character_sem_espaco=character_sem_espaco,
+                            sum_character=sum_character,
+                            sum_palavras=sum_palavras))
 
 
 app.run(debug=True)
